3b0e078497559d5001028ffad724f3f154f492bd
Junrong <87742302+akiraafu@users.noreply.github.com>
2022-10-08 15:06:33 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const backToTopButton = document.querySelector(".btn");
+const backToTopButton = document.querySelector(".toTopBtn");
 
 document.addEventListener("scroll", () => {
   backToTopButton.classList.toggle("active", window.scrollY > 0);
